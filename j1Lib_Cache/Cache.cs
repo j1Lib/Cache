0e09e4c78fb41fedcef9fedf6a4fdaeb032c97f0
j113203 <j113203@users.noreply.github.com>
2016-07-01 13:00:15 +0800

    Add files via upload

---
+++ j1Lib_Cache/Cache.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace j1Lib
+{
+    public static class Cache
+    {
+        private static Dictionary<string, object> cache = new Dictionary<string, object>();
+
+        private static string getKey(object target, object key)
+        {
+            return target.ToString() + "_" + key + "_" + target.ToString();
+        }
+
+        public static void setCache(this object target, string key, object value)
+        {
+            if (cache.ContainsKey(getKey(target, key)))
+            {
+                cache[getKey(target, key)] = value;
+            }
+            else
+            {
+                cache.Add(getKey(target, key), value);
+            }
+        }
+
+        public static object getCache(this object target, string key, object defaults)
+        {
+            if (cache.ContainsKey(getKey(target, key)))
+            {
+                return cache[getKey(target, key)];
+            }
+            else
+            {
+                return defaults;
+            }
+        }
+    }
+}
